acf975e2342815d4e1d57a92c98581f7a8cc1c9e
FuckingToasters <94435104+FuckingToasters@users.noreply.github.com>
2022-06-18 13:18:30 +0200

    Update main.py
    - Added verified tokens saved in a extra file
    - Fixed Threading & issues when choosing more than 1 Thread
    - Improved Code Design for a better understanding

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ CAPTCHA_KEY = config["Captcha Stuff"]["Captcha Service ApiKey"]
 
 def print_main_menu(): return main_menu.logo()
 def verify(proxy_type, tzid=None, number=None):
+    lock = threading.Lock()
     with open("files/proxies.txt", "r") as proxy_file:
         proxies = proxy_file.read().splitlines()
         try:
@@ -29,9 +30,10 @@ def verify(proxy_type, tzid=None, number=None):
 
     with open("files/tokens.txt", "r+") as token_file:
         try: line = random.choice(token_file.readlines()).split(":")
-        except IndexError: pystyle.Write.Print("\t[*] Make sure that the Tokens inside tokens.txt are formatted correctly (token:password)!\n", pystyle.Colors.yellow, interval=0), sys.exit(1)
+        except IndexError: pystyle.Write.Print("\t[*] Tokens inside tokens.txt are not formatted correctly (token:password)!\n", pystyle.Colors.yellow, interval=0), sys.exit(1)
         token, password = line[0], line[1]
         tokencombo = line[0] + ":" + line[1]
+
     headers = {
         "accept": "*/*",
         "accept-encoding": "gzip, deflate, br",
@@ -64,17 +66,21 @@ def verify(proxy_type, tzid=None, number=None):
                 for item in lines:
                     if item != tokencombo: token_file.write(item)
                 token_file.truncate()
-            verify()
+            verify(proxy_type=proxy_type)
 
     except KeyError:
         if "id" in check_token.json():
             pystyle.Write.Print(f"\t[+] Valid Token {token}!\n", pystyle.Colors.green, interval=0)
-
             resp1 = httpx.get(f"https://onlinesim.ru/api/getNum.php?apikey={APIKEY}&service=discord&number=true&country={COUNTRY}")
+
+            while resp1.json()["response"] == "INTERVAL_CONCURRENT_REQUESTS_ERROR":
+                resp1 = httpx.get(f"https://onlinesim.ru/api/getNum.php?apikey={APIKEY}&service=discord&number=true&country={COUNTRY}")
+                time.sleep(4)
+
             if "tzid" in resp1.json():
                 pystyle.Write.Print(f"\t[+] Sucessfully got a Number from https://onlinesim.ru!\n", pystyle.Colors.green, interval=0)
                 tzid, number = resp1.json()["tzid"], resp1.json()["number"]
-
+            lock.acquire()
             if resp1.json()["response"] == "ACCOUNT_BLOCKED": pystyle.Write.Print(f"\t[-] Your Account has been blocked. Please create a new onlinesim account & use your new API Key!\n", pystyle.Colors.red, interval=0), sys.exit(1)
             elif resp1.json()["response"] == "ERROR_WRONG_KEY": pystyle.Write.Print("\t[-] Wrong api key provided!\n", pystyle.Colors.red, interval=0), sys.exit(1)
             elif resp1.json()["response"] == "ERROR_NO_KEY": pystyle.Write.Print("\t[-] We couldn't find a API Key!\n", pystyle.Colors.red, interval=0), sys.exit(1)
@@ -83,28 +89,29 @@ def verify(proxy_type, tzid=None, number=None):
             elif resp1.json()["response"] == "API_ACCESS_DISABLED": pystyle.Write.Print("\t[-] Your API Access has been disabled!\n", pystyle.Colors.red, interval=0), sys.exit(1)
             elif resp1.json()["response"] == "API_ACCESS_IP": pystyle.Write.Print("\t[-] The Access from this IP has been disabled in your Profile!\n", pystyle.Colors.red, interval=0), sys.exit(1)
             elif resp1.json()["response"] == "WARNING_LOW_BALANCE": pystyle.Write.Print("\t[-] This order can't be placed, the Account Balance is too low!\n", pystyle.Colors.red, interval=0), sys.exit(1)
-
-
             pystyle.Write.Print("\t[*] Solving captcha... please be patient!\n", pystyle.Colors.yellow, interval=0)
             solver = captchatools.captcha_harvesters(solving_site=CAPTCHA_SERVICE, api_key=CAPTCHA_KEY, sitekey="f5561ba9-8f1e-40ca-9b5b-a0b3f719ef34", captcha_url="https://discord.com/api/v9/users/@me/phone")
             captcha_token = solver.get_token()
+            lock.release()
             data1 = {"captcha_key": captcha_token, "change_phone_reason": "user_settings_update", "phone": number}
             resp2 = httpx.post("https://discord.com/api/v9/users/@me/phone", json=data1, headers=headers, proxies=proxy_auth)
-
             if json.decoder.JSONDecodeError: pass
             if resp2.status_code == 204:
                 pystyle.Write.Print("\t[+] Successfully requested verification code!\n", pystyle.Colors.green, interval=0)
             time.sleep(4)
 
             resp3 = httpx.get(f"https://onlinesim.ru/api/getState.php?apikey={APIKEY}&tzid={tzid}&message_to_code=1")
-            if resp3.json()[0]["response"] == "WARNING_NO_NUMS": pystyle.Write.Print("\t[*] No matching numbers found!\n", pystyle.Colors.yellow, interval=0)
-            elif resp3.json()[0]["response"] == "TZ_INPOOL": ppystyle.Write.Print("\t[*] Waiting for a number to be dedicated to the operation!\n", pystyle.Colors.yellow, interval=0)
-            elif resp3.json()[0]["response"] == "TZ_NUM_ANSWER": pystyle.Write.Print("\t[*] SMS Code has arrived!\n", pystyle.Colors.yellow, interval=0)
-            elif resp3.json()[0]["response"] == "TZ_OVER_EMPTY": pystyle.Write.Print("\t[*] SMS Code did not arrive within the specified time!\n", pystyle.Colors.yellow, interval=0)
-            elif resp3.json()[0]["response"] == "TZ_OVER_OK": pystyle.Write.Print("\t[*] The operation has been completed!\n", pystyle.Colors.yellow, interval=0)
-            elif resp3.json()[0]["response"] == "ERROR_NO_TZID": pystyle.Write.Print("\t[*] The tzid is not specified!\n", pystyle.Colors.yellow, interval=0)
-            elif resp3.json()[0]["response"] == "ERROR_NO_OPERATIONS": pystyle.Write.Print("\t[*] No operations found!\n", pystyle.Colors.yellow, interval=0)
-            elif resp3.json()[0]["response"] == "ACCOUNT_IDENTIFICATION_REQUIRED": pystyle.Write.Print("\tYou have to go through an identification process: to order a messenger - in any way, for forward - on the passport!\n", pystyle.Colors.yellow, interval=0)
+            try:
+                if resp3.json()[0]["response"] == "WARNING_NO_NUMS": pystyle.Write.Print("\t[*] No matching numbers found!\n", pystyle.Colors.yellow, interval=0)
+                elif resp3.json()[0]["response"] == "TZ_INPOOL": ppystyle.Write.Print("\t[*] Waiting for a number to be dedicated to the operation!\n", pystyle.Colors.yellow, interval=0)
+                elif resp3.json()[0]["response"] == "TZ_NUM_ANSWER": pystyle.Write.Print("\t[*] SMS Code has arrived!\n", pystyle.Colors.yellow, interval=0)
+                elif resp3.json()[0]["response"] == "TZ_OVER_EMPTY": pystyle.Write.Print("\t[*] SMS Code did not arrive within the specified time!\n", pystyle.Colors.yellow, interval=0)
+                elif resp3.json()[0]["response"] == "TZ_OVER_OK": pystyle.Write.Print("\t[*] The operation has been completed!\n", pystyle.Colors.yellow, interval=0)
+                elif resp3.json()[0]["response"] == "ERROR_NO_TZID": pystyle.Write.Print("\t[*] The tzid is not specified!\n", pystyle.Colors.yellow, interval=0)
+                elif resp3.json()[0]["response"] == "ERROR_NO_OPERATIONS": pystyle.Write.Print("\t[*] No operations found!\n", pystyle.Colors.yellow, interval=0)
+                elif resp3.json()[0]["response"] == "ACCOUNT_IDENTIFICATION_REQUIRED": pystyle.Write.Print("\tYou have to go through an identification process: to order a messenger - in any way, for forward - on the passport!\n", pystyle.Colors.yellow, interval=0)
+
+            except KeyError: pass
 
             pystyle.Write.Print("[*] Waiting for the SMS Code...!\n", pystyle.Colors.yellow, interval=0)
             while resp3.json()[0]["response"] == "TZ_NUM_WAIT":
@@ -125,10 +132,19 @@ def verify(proxy_type, tzid=None, number=None):
             with open("files/verifiedtoken.txt", "w+") as verified_file:
                 verified_file.write(tokencombo + "\n")
 
+            with open("files/tokens.txt", "r+") as token_file:
+                lines = token_file.readlines()
+                token_file.seek(0)
+                for item in lines:
+                    if item != tokencombo: token_file.write(item)
+                token_file.truncate()
+            verify(proxy_type=proxy_type)
+
 if __name__ == "__main__":
     print_main_menu()
-    session_input = pystyle.Write.Input("[**] How many concurrent Threads do you want to use?: ", pystyle.Colors.cyan, interval=0).lower()
+    session_input = pystyle.Write.Input("\t[**] How many concurrent Threads do you want to use?: ", pystyle.Colors.cyan, interval=0).lower()
     proxy_input = pystyle.Write.Input("\t[**] Proxy Type (http/https/socks5) | Enter nothing to use without Proxy: ", pystyle.Colors.cyan, interval=0).lower()
+    if proxy_input == "http": proxy_input = "https"
     threads = []
     try:
         for _ in range(int(session_input)):
