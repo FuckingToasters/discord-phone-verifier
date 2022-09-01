242b8fee3321aa28ae663cd3905e7925fe7bc8c2
FuckingToasters <94435104+FuckingToasters@users.noreply.github.com>
2022-09-01 22:21:25 +0200

    Update hcaptchasolver.py

--- new version/plugins/captcha/hcaptchasolver.py
+++ new version/plugins/captcha/hcaptchasolver.py
@@ -4,7 +4,7 @@ from plugins.configuration.load import config
 
 class bypasscaptcha():
     def __init__(self):
-        self.CAPTCHASERVICE, self.CAPTCHAKEY, self.SITEKEY, _, _, _, _, _, _ = config().loadconfig()
+        self.CAPTCHASERVICE, self.CAPTCHAKEY, self.SITEKEY, _, _, _, _, _, _, _ = config().loadconfig()
 
     def hcaptcha(self):
         solver = captchatools.captcha_harvesters(solving_site=self.CAPTCHASERVICE, api_key=self.CAPTCHAKEY, captcha_type="hcaptcha", sitekey=self.SITEKEY, captcha_url="https://discord.com/api/v9/users/@me/phone")
