be3693df7a0f1a64c035cdccd5fab44e4e8ea608
FuckingToasters <94435104+FuckingToasters@users.noreply.github.com>
2022-11-12 18:35:37 +0100

    Update vaksms.py
    small bug fix 🐛

--- new version/plugins/phoneservices/vaksms.py
+++ new version/plugins/phoneservices/vaksms.py
@@ -50,6 +50,7 @@ class vakverification:
         self.COUNTRY = country(self)
 
     def ordernumber(self):
+        if self.OPERATOR == "any": self.OPERATOR = "" # vaksms specific
         url = f"https://vak-sms.com/api/getNumber/?apiKey={self.APIKEY}&service=dc&country={self.COUNTRY}&operator={self.OPERATOR}&softId=34"
         with requests.Client(headers=None) as client: response = client.get(url).json()
         self.NUMBER, self.TZID = str(response["tel"]), response["idNum"]
@@ -70,15 +71,13 @@ class vakverification:
         url = f"https://vak-sms.com/api/getStatus/?apiKey={self.APIKEY}&idNum={self.TZID}"
         discordurl = "https://discord.com/api/v9/users/@me/phone"
 
-        with requests.Client(headers=self.HEADERS) as client: response = client.get(url).json()
-
-        # {'id': 376167805, 'phone': '+79217195992', 'operator': 'megafon', 'product': 'discord', 'price': 4, 'status': 'PENDING', 'expires': '2022-11-03T11:07:07.153905Z', 'sms': [], 'created_at': '2022-11-03T10:52:07.153905Z', 'country': 'russia'}
+        with requests.Client() as client: response = client.get(url).json()
         while response["smsCode"] is None: 
             waitcount += 1
 
             pystyle.Write.Print(f"\t[*] Discord haven't sent the SMS so far... {waitcount}/120!\n", pystyle.Colors.yellow, interval=0)
             with requests.Client(timeout=self.TIMEOUT) as client:
-                response = client.get(url, headers=self.HEADERS).json()
+                response = client.get(url).json()
                 time.sleep(.3)
             
             if waitcount >= 120:
