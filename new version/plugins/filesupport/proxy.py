16388c3062d7d07b0bb071f709439ecb867fbd3f
FuckingToasters <94435104+FuckingToasters@users.noreply.github.com>
2022-11-12 14:35:22 +0100

    Update proxy.py

--- new version/plugins/filesupport/proxy.py
+++ new version/plugins/filesupport/proxy.py
@@ -4,17 +4,19 @@ class loadproxyclass:
         with open("files/proxies.txt", "r") as proxy_file:
             proxies = proxy_file.read().splitlines()
             proxy_split = "None"
+            proxyauth, hostname, port, username, password = None, None, None, None, None
+
             for proxy in proxies: proxy_split = proxy.split(":")
             try:
-                host_name, port, username, password = proxy_split[0], proxy_split[1], proxy_split[2], proxy_split[3]
-                proxy_formatted = f"{proxytype}://{username}:{password}@{host_name}:{port}"
+                hostname, port, username, password = proxy_split[0], proxy_split[1], proxy_split[2], proxy_split[3]
+                proxy_formatted = f"{proxytype}://{username}:{password}@{hostname}:{port}"
                 if proxytype != "": proxyauth = {"all://": proxy_formatted}
                 else: proxyauth = {"all://": None}
 
             except IndexError:
                 try:
-                    host_name, port = proxy_split[0], proxy_split[1]
-                    proxy_formatted = f"{proxytype}://{host_name}:{port}"
+                    hostname, port, username, password = proxy_split[0], proxy_split[1], None, None
+                    proxy_formatted = f"{proxytype}://{hostname}:{port}"
                     if proxytype != "": proxyauth = {"all://": proxy_formatted}
                     else: proxyauth = {"all://": None}
 
@@ -22,4 +24,4 @@ class loadproxyclass:
                     pystyle.Write.Print("\t[*] Proxies inside files/proxies.txt are not formatted correctly (IP:PORT or IP:PORT:USER:PASS)!\n", pystyle.Colors.yellow, interval=0)
                     proxyauth = {"all://": None}
             
-            return proxyauth
+            return proxyauth, hostname, port, username, password
