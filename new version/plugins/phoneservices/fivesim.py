aaacbac3bb797ff5e7ac54fde6163f23ce2b346e
FuckingToasters <94435104+FuckingToasters@users.noreply.github.com>
2022-11-17 13:28:38 +0100

    Update fivesim.py
    fixed issue with loading smshub api in fivesim.py instead of 5sim api

--- new version/plugins/phoneservices/fivesim.py
+++ new version/plugins/phoneservices/fivesim.py
@@ -5,7 +5,7 @@ from plugins.configuration.load import config
 
 class fivesimverification:
     def __init__(self):
-        _, _, _, _, self.OPERATOR, _, _, _, _, _, self.APIKEY, self.COUNTRY, _ = config().loadconfig()
+        _, _, _, _, _, self.OPERATOR, _, _, self.APIKEY, self.COUNTRY, _, _, _ = config().loadconfig()
         self.COUNTRY = self.COUNTRY.lower() if self.COUNTRY is str else self.COUNTRY
         self.NUMBER = None
         self.TZID = None
@@ -16,15 +16,15 @@ class fivesimverification:
     def ordernumber(self):
         url = f"https://5sim.net/v1/user/buy/activation/{self.COUNTRY}/any/discord"
         with requests.Client(headers=self.HEADERS) as client: response = client.get(url)
-        if response.status_code == 400: 
+        if response.status_code == 400:
             pystyle.Write.Print(f"\t[*] Could not order the Number ({response.text})!\n", pystyle.Colors.yellow, interval=0)
             time.sleep(.3)
             fivesimverification.ordernumber(self)
-        
-        if response.status_code == 200: 
+
+        if response.status_code == 200:
             self.NUMBER, self.TZID = f"+{response.json()['phone']}", response.json()["id"]
             return self.NUMBER, self.TZID
-    
+
 
     def deletenumber(self):
         url = f"https://5sim.net/v1/user/cancel/{self.TZID}"
@@ -34,7 +34,7 @@ class fivesimverification:
         # with requests.Client(headers=None) as client: response = client.get(url).json()
         # if response["status"] == "update":  self.DELETED = True
         # return self.DELETED
-    
+
 
     def getcode(self):
         waitcount = 0
@@ -50,11 +50,11 @@ class fivesimverification:
             with requests.Client(timeout=self.TIMEOUT) as client:
                 response = client.get(url, headers=self.HEADERS).json()
                 time.sleep(.3)
-            
+
             if waitcount >= 120:
                 return "TIMEOUT", False
-        
+
         print(response)
         self.VERIFYCODE = response["sms"][0]["code"] if response["status"] == "FINISHED" else None
         return waitcount, self.VERIFYCODE
-    
+
