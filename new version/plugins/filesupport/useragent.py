252e4a07ff12e245c3e03753a9e2827463bf9ce5
FuckingToasters <94435104+FuckingToasters@users.noreply.github.com>
2022-12-30 00:54:47 +0100

    Delete new version directory

--- new version/plugins/filesupport/useragent.py
+++
@@ -1,8 +0,0 @@
-import random
-
-class randomagentclass:
-    def randomagent(self):
-        with open("files/useragents.txt") as ua_file:
-            self.LINES = ua_file.readlines()
-            self.USERAGENT = random.choice(self.LINES).strip()
-        return self.USERAGENT
